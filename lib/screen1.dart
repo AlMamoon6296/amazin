20fe6fb735129e82500f34a579cf274605771721
vitwo6Livo <sayedalmamoon@gmail.com>
2023-11-06 17:19:19 +0530

    second commit

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,29 +1,16 @@
-import 'package:amazin/screen2.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
+import 'package:amazin/screen2.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late File _image;
-
-  Future getImageFromCamera() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-
-    if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
-    }
-  }
-
   List<Map<String, dynamic>> load = [
     {
       'image': 'assets/images/asusVivobook.jpg',
@@ -67,7 +54,57 @@ class _MyHomePageState extends State<MyHomePage> {
       'tile':
           'Graphics are powered by Nvidia GeForce MX150. Connectivity options include Bluetooth, Ethernet and it comes with 4 USB ports (2 x USB 2.0, 1 x USB 3.0), Multi Card Slot, Mic In ports.'
     },
+    {
+      'image': 'assets/images/laptop.jpg',
+      'deal': 'Deal Of the Day',
+      'festival': 'Great Indian Festival',
+      'tile':
+          'Graphics are powered by Nvidia GeForce MX150. Connectivity options include Bluetooth, Ethernet and it comes with 4 USB ports (2 x USB 2.0, 1 x USB 3.0), Multi Card Slot, Mic In ports.'
+    },
+    {
+      'image': 'assets/images/laptop.jpg',
+      'deal': 'Deal Of the Day',
+      'festival': 'Great Indian Festival',
+      'tile':
+          'Graphics are powered by Nvidia GeForce MX150. Connectivity options include Bluetooth, Ethernet and it comes with 4 USB ports (2 x USB 2.0, 1 x USB 3.0), Multi Card Slot, Mic In ports.'
+    },
+    {
+      'image': 'assets/images/laptop.jpg',
+      'deal': 'Deal Of the Day',
+      'festival': 'Great Indian Festival',
+      'tile':
+          'Graphics are powered by Nvidia GeForce MX150. Connectivity options include Bluetooth, Ethernet and it comes with 4 USB ports (2 x USB 2.0, 1 x USB 3.0), Multi Card Slot, Mic In ports.'
+    },
+    {
+      'image': 'assets/images/CHUWI.jpg',
+      'deal': 'Deal Of the Day',
+      'festival': 'Great Indian Festival',
+      'tile':
+          'Graphics are powered by Nvidia GeForce MX150. Connectivity options include Bluetooth, Ethernet and it comes with 4 USB ports (2 x USB 2.0, 1 x USB 3.0), Multi Card Slot, Mic In ports.'
+    },
   ];
+
+  void deleteItem(int index) {
+    setState(() {
+      load.removeAt(index);
+    });
+  }
+
+  Future<void> openCamera() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        load.add({
+          'image': pickedFile.path,
+          'deal': 'New Item',
+          'festival': 'Latest Addition',
+          'tile': 'Brand new item added to the list.',
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,103 +121,111 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           child: TextField(
             decoration: InputDecoration(
-                // fillColor: Colors.red,
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                //labelText: 'Search Amazon.in',
-                hintText: 'Search Amazon.in'),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              hintText: 'Search Amazon.in',
+            ),
           ),
         ),
         actions: [
           IconButton(
             icon: Icon(Icons.camera),
-            onPressed: getImageFromCamera,
+            onPressed: openCamera,
           ),
         ],
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+        ),
       ),
       body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 20.0,
-            mainAxisSpacing: 20.0,
-          ),
-          itemCount: load.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                // Navigate to the next page when an item is tapped
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ShowDescription(receivedList: load),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20.0,
+          mainAxisSpacing: 20.0,
+        ),
+        itemCount: load.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ShowDescription(
+                    receivedList: load,
+                    currentIndex: index,
+                    onDelete: deleteItem,
                   ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        flex: 10,
-                        fit: FlexFit.tight,
-                        child: Image.asset(
-                          load[index]['image'],
-                          height: 100,
-                        ),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 10,
+                      fit: FlexFit.tight,
+                      child: Image.asset(
+                        load[index]['image'],
+                        height: 100,
                       ),
-                      // SizedBox(
-                      //   height: 5,
-                      //),
-                      Flexible(
-                        flex: 2,
-                        fit: FlexFit.tight,
-                        child: Container(
-                          color: Colors.red,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 2),
-                            child: Text(
-                              load[index]['deal'],
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 9),
-                            ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      fit: FlexFit.tight,
+                      child: Container(
+                        color: Colors.red,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 2),
+                          child: Text(
+                            load[index]['deal'],
+                            style: TextStyle(color: Colors.white, fontSize: 9),
                           ),
                         ),
                       ),
-                      Flexible(
-                        flex: 2,
-                        fit: FlexFit.tight,
-                        child: Text(
-                          load[index]['festival'],
-                          style: TextStyle(color: Colors.red),
-                        ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      fit: FlexFit.tight,
+                      child: Text(
+                        load[index]['festival'],
+                        style: TextStyle(color: Colors.red),
                       ),
-                      Flexible(
-                          flex: 4,
-                          fit: FlexFit.tight,
-                          child: Text(
-                            load[index]['tile'],
-                            style: TextStyle(fontSize: 15, color: Colors.white),
-                            maxLines: 2,
-                          )),
-                      Flexible(
-                          flex: 2,
-                          fit: FlexFit.tight,
-                          child: ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.white,
-                                onPrimary: Colors.red,
-                              ),
-                              child: Icon(Icons.delete)))
-                    ],
-                  ),
+                    ),
+                    Flexible(
+                      flex: 4,
+                      fit: FlexFit.tight,
+                      child: Text(
+                        load[index]['tile'],
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      fit: FlexFit.tight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          deleteItem(index);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.red,
+                        ),
+                        child: Icon(Icons.delete),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          }),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,26 +1,51 @@
+// import 'package:flutter/material.dart';
+
+// class ShowDescription extends StatelessWidget {
+//   final List<Map<String, dynamic>> receivedList;
+
+//   ShowDescription(
+//       {Key? key,
+//       required this.receivedList,
+//       required int currentIndex,
+//       required void Function(int index) onDelete})
+//       : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           Text(receivedList[currentIndex]['deal']),
+//           Text(receivedList[currentIndex]['title']),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class ShowDescription extends StatelessWidget {
   final List<Map<String, dynamic>> receivedList;
+  final int currentIndex;
+  final void Function(int index) onDelete;
 
-  ShowDescription({Key? key, required this.receivedList}) : super(key: key);
+  ShowDescription({
+    Key? key,
+    required this.receivedList,
+    required this.currentIndex,
+    required this.onDelete,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: receivedList.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Image.asset(receivedList[index]['image']),
-
-              //Text(receivedList[index]['deal']),
-              //Text(receivedList[index]['festival']),
-              Text(receivedList[index]['tile']),
-            ],
-          );
-        },
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Image.asset(receivedList[currentIndex]['image']),
+            Text(receivedList[currentIndex]['tile']),
+          ],
+        ),
       ),
     );
   }
